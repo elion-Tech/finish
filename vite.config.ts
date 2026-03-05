97491c4e73eba7456080ea75169e3f2f3f296088
elion-Tech <igweokwuekene@gmail.com>
2026-03-05 17:25:27 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,8 @@ export default defineConfig(({mode}) => {
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
+    preview: {
+      host: true, // Expose server to the network
+    },
   };
 });
